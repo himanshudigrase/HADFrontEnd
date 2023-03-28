9841a6bf48f3e01e4e1fd8b4de81d4b897565bd7
himanshudigrase <himanshudigrase@gmail.com>
2023-03-28 22:24:05 +0530

    Done FrontEnd changes

--- services/questions.js
+++ services/questions.js
@@ -1,18 +1,16 @@
 import axios from 'axios'
 import { commonUrl } from './commonUrl'
 // The API endpoint where login data is sent to
-const assignmentsUrl = commonUrl + `/assignment/`
+const questionsUrl = commonUrl + `/assignment/`
 
 const postAssignment = async (assignmentNumber,user) => {
   // Send the assignmentId to the assignmentsUrl API endpoint as an HTTP GET request
   // Note the async-await
   const response = await axios.post(user + assignmentsUrl + 'add', assignmentNumber)
-  
+ 
   return response.data
 }
 
-
-
 // Export the method as an object so that it can be accessible outside this file as a service
-const exportObject = { postAssignment }
+const exportObject = { getAssignment }
 export default exportObject
